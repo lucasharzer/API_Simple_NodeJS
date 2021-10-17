674a86f1cdad59bfc9fdcaa71c78fac20c385ce0
Lucas de Matos <lucasmatos592@gmail.com>
2021-10-17 20:53:15 -0300

    API Simple NodeJS

---
+++ index.js
@@ -0,0 +1,21 @@
+// API Simple NodeJS
+
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+
+app.listen(18062, () => {
+    console.log(`Express started at http://localhost:18062`)
+})
